1ba4379ade42dcb24b7b8279bcf2152bd1050377
白影 <356741418@qq.com>
2025-09-05 21:00:33 +0800

    Revert "clean"
    This reverts commit 559347c2ad86590e8b540720553b54a5aa9c9cce.

---
+++ core/admin.py
@@ -0,0 +1,34 @@
+#  Copyright (c) 2025. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+#  Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+#  Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+#  Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+#  Vestibulum commodo. Ut rhoncus gravida arcu.
+from django.contrib import admin
+
+# Register your models here.
+from core.models import Post, Account, Comment, LikePost, LikeComment
+from core.models import Tag
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(LikePost)
+class LikePostAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(LikeComment)
+class LikeCommentAdmin(admin.ModelAdmin): ...
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin): ...
